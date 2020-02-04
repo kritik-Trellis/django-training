50c73a639830da832816b8851d5c0bce74bbdcf2
kritik-trellis <akritik@trellissoft.com>
2020-02-04 16:26:02 +0530

    Added Word Count Project

---
+++ wordcount-project/wordcount/views.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+def Home(request):
+    return render(request,'home.html')
+
+def countfile(request):
+    worddict={}
+    if request.method=="POST":
+        uploadFile=request.FILES['myfile']
+        file_name=uploadFile.name
+        file_size=uploadFile.size
+        txt=uploadFile.read().decode('utf-8')
+        for word in txt.split():
+            if word in worddict:
+                worddict[word] +=1
+            else:
+                worddict[word] =1
+        sortedword=sorted(worddict.items(),key=operator.itemgetter(1),reverse=True)
+        return render(request,'countfile.html',{'Text':txt,'FILE_NAME':file_name,'len':len(txt.split()),'worddict':sortedword})
+
+
+
+def count(request):
+    worddict={}
+    fulltext=request.GET['fulltext']
+    wordList=fulltext.split()
+
+    for word in wordList:
+        if word in worddict:
+            worddict[word] +=1
+        else:
+            worddict[word] =1
+    sortedword=sorted(worddict.items(),key=operator.itemgetter(1),reverse=True)
+
+
+    return render(request,'count.html',{'fulltext':fulltext,'count':len(wordList),'worddict':sortedword})
+
+def about(request):
+    return render(request,'about.html')
