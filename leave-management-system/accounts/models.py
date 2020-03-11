771b0edfe54e3eec8d80300d090a5e5139fe73e9
kritik-trellis <akritik@trellissoft.com>
2020-03-11 13:02:08 +0530

    a

--- leave-management-system/accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class UserModel(models.Model):
-    email = models.EmailField(max_length = 50)
-    password = models.CharField(max_length = 30)
-
-    def __str__(self):
-        return self.email
--- leave-management-system/lms/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from accounts import views
-from django.contrib import admin
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.login,name="login"),
-    path('accounts/',include('accounts.urls'))
-]
